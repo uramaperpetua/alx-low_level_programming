de664cd3c341d285958fc8396906a799f288069c
uramaperpetua <uramaperpetua@gmail.com>
2022-06-03 13:59:52 +0000

    done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, b);
-	if (n == 0)
+	else if (n == 0)
 		printf("Last digit of %d is %d and is 0\n", n, b);
 	if (n != 0 && n < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, b);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
