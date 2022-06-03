1da2ff6e225f02ea4078e4cf1536a797b549f4d0
uramaperpetua <uramaperpetua@gmail.com>
2022-06-03 15:48:53 +0000

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char ch = 'a';
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
