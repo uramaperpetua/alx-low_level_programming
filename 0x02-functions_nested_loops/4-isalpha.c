bc92d2a244803510423a1c6ba484341729c41de0
uramaperpetua <uramaperpetua@gmail.com>
2022-06-07 22:56:59 +0000

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: Entry point
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+
+		}
+	return (0);
+}
