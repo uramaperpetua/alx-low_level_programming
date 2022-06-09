643415e38c911c8f8848f7045955c92945b82999
uramaperpetua <uramaperpetua@gmail.com>
2022-06-09 21:07:48 +0000

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,11 @@
  */
 void print_numbers(void)
 {
-	int i;
+	int a;
 
-	for (i = 0; i < 10; i++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		_putchar(i + '0');					
+		_putchar(a);
 	}
-
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
