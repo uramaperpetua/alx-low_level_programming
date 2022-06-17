7924ac0a7929cbf1ae22a75afe3d897fad1da71c
uramaperpetua <uramaperpetua@gmail.com>
2022-06-17 00:07:05 +0000

    done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,13 +6,13 @@ int main(void)
 	int a[5];
 	int *p;
 
-        a[2] = 1024;
+	a[2] = 1024;
 	p = &n;
 	/*
 	 * write your line of code here...
 	 * Remember:
-	 * - you are not allowed to use a	
-     	 * - you are not allowed to modify p
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
