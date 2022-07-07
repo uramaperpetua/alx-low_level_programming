841b22733013926f2e71a6a0f84c2cbb743b996a
uramaperpetua <uramaperpetua@gmail.com>
2022-07-07 11:33:40 +0000

    done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,13 +18,16 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(li, n);
 
-	if (n == 0)
-		return (0);
-
-	if (n != 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
 			sum += va_arg(li, int);
+		}
 	}
 
 	va_end(li);
